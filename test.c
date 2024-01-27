6e5fd19f0a17463e6dfd00c1455904e848573978
linbei-pro <148841405+linbei-pro@users.noreply.github.com>
2024-01-27 10:57:10 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,25 @@
+//求一个整数在内存存储中的二进制中1的个数
+
+//就是求补码中的二进制中1的个数
+#include<stdio.h>
+
+int main()
+{
+	int n = 0, m = 0;
+	int i = 0;
+	scanf_s("%d", &n);
+
+	int count = 0;
+	for (i = 32; i > 0; i--)
+	{
+		m = n & 1;
+		if (m == 1)
+		{
+			count++;
+		}
+		n = n >> 1;
+		m = 0;
+	}
+	printf("%d", count);
+	return 0;
+}
